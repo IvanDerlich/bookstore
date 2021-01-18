1a9d1a2d8030b3d7c914ed7ccb29d00555044940
Ivan Derlich <e@ivanderlich.com>
2021-01-18 14:48:59 -0300

    Create navigation links

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+import {
+  BrowserRouter, Link, Route, Switch,
+} from 'react-router-dom';
 import './App.css';
 import BooksList from '../containers/BookList';
 import BookForm from '../containers/BookForm';
@@ -5,8 +9,24 @@ import BookForm from '../containers/BookForm';
 function App(/* props */) {
   return (
     <div className="App">
-      <BookForm />
-      <BooksList />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/categories">Categories</Link></li>
+            <li><Link to="/books">Books</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/categories">
+            <div>Categories</div>
+          </Route>
+          <Route path="/books">
+            <div>Books</div>
+          </Route>
+        </Switch>
+        <BookForm />
+        <BooksList />
+      </BrowserRouter>
     </div>
   );
 }
